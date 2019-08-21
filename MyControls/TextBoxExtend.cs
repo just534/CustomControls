0ada3c7af95e7c9b04b721792916447cc4510f82
just534 <273510287@qq.com>
2019-08-21 13:26:24 +0800

    添加项目文件。

---
+++ MyControls/TextBoxExtend.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyControls
+{
+    public partial class TextBoxExtend : TextBox
+    {
+        public TextBoxExtend()
+        {
+            InitializeComponent();
+        }
+
+        public TextBoxExtend(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
